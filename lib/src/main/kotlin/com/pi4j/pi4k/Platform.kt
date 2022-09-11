f1eda572db533683ab5ec9ab249016e0885d0d5d
Muhammad Hashim <msg@mhashim6.me>
2022-09-11 17:14:43 +0200

    Platform generics + tests

--- lib/src/main/kotlin/com/pi4j/pi4k/Platform.kt
+++ lib/src/main/kotlin/com/pi4j/pi4k/Platform.kt
@@ -1,7 +1,15 @@
 package com.pi4j.pi4k
 
+import com.pi4j.io.IO
+import com.pi4j.io.IOConfig
+import com.pi4j.platform.Platform
+import com.pi4j.provider.Provider
+
 /**
  * @author Muhammad Hashim (mhashim6) (<a href="https://mhashim6.me">https://mhashim6.me</a>)
  */
 
-// TODO reified types extensions
+inline fun <reified T : Provider<*, *, *>> Platform.hasProvider() = hasProvider(T::class.java)
+inline fun <reified T : Provider<*, *, *>> Platform.provider(): T = provider(T::class.java)
+inline fun <reified I : IO<*, *, *>> Platform.createFrom(config: IOConfig<*>): I = create(config, I::class.java )
+inline fun <reified I : IO<*, *, *>> Platform.createFrom(id: String): I = create(id, I::class.java )
---
+++ lib/src/test/kotlin/com/pi4j/pi4k/PlatformTest.kt
@@ -0,0 +1,39 @@
+package com.pi4j.pi4k
+
+import com.pi4j.Pi4J
+import com.pi4j.context.Context
+import com.pi4j.plugin.mock.platform.MockPlatform
+import com.pi4j.plugin.mock.provider.pwm.MockPwmProvider
+import org.junit.jupiter.api.Test
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.assertEquals
+import kotlin.test.assertSame
+
+/**
+ * @author Muhammad Hashim (mhashim6) (<a href="https://mhashim6.me">https://mhashim6.me</a>)
+ */
+
+internal class PlatformTest {
+
+    private lateinit var context: Context
+
+    @BeforeTest
+    fun setup(){
+        context = Pi4J.newAutoContext()
+    }
+
+    @Test
+    fun `test generics`() {
+        context.platform<MockPlatform>() .run {
+            assertEquals(hasProvider(MockPwmProvider::class.java), hasProvider<MockPwmProvider>())
+            assertSame(provider(MockPwmProvider::class.java), provider<MockPwmProvider>())
+            assertSame(getProvider("mock-pwm", MockPwmProvider::class.java), getProvider<MockPwmProvider>("mock-pwm"))
+        }
+    }
+
+    @AfterTest
+    fun tearDown(){
+        context.shutdown()
+    }
+}
