a53e9cd401d5b292ee9c015bda23accd6d61aa5e
Muhammad Hashim <msg@mhashim6.me>
2022-09-12 19:35:09 +0200

    prepare publication

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.7.10"
+    `java-library`
+    `maven-publish`
 }
 val libVersion = "0.2"
 val libName = "ktx"
@@ -23,10 +25,69 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+publishing {
+    repositories {
+        maven {
+            name = "Local"
+            url = uri("file://${buildDir}/local-repository")
+        }
+    }
+    publications {
+        create<MavenPublication>(libName) {
+            groupId = "com.pi4j"
+            artifactId = libName
+            version = libVersion
+            from(components["java"])
+            pom {
+                version = libVersion
+                artifactId = libName
+                name.set("Pi4J-Kotlin")
+                description.set("Kotlin DSL for Pi4J V2")
+                url.set("https://github.com/Pi4J/pi4j-kotlin")
+                organization {
+                    name.set("Pi4J")
+                    url.set("https://pi4j.com")
+                }
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mhashim6")
+                        name.set("Muhammad Hashim")
+                        email.set("msg@mhashim6.me")
+                        organization.set("mhashim6")
+                        organizationUrl.set("https://mhashim6.me")
+                        properties.run {
+                            put("blog", "https://blog.mhashim6.me")
+                            put("linkedin", "https://linkedin.com/in/mhashim6")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Name" to "Pi4J-Kotlin",
+            "Organisation" to "Pi4J",
+            "URL" to "https://github.com/Pi4J/pi4j-kotlin",
+            "Author" to "mhashim6",
+            "Author-Website" to "https://mhashim6.me"
+        )
+    }
 }
