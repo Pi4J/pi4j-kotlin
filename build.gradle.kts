b81c937a28d8e2364f5ae8091ee8bd365054bba1
Muhammad Hashim <msg@mhashim6.me>
2022-09-14 18:51:37 +0200

    sonatype gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 group = "com.pi4j"
@@ -21,4 +22,10 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
 }
