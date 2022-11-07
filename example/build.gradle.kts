3c61ecf57c8b2ee0a33219dba5d15ee51a171725
Muhammad Hashim <msg@mhashim6.me>
2022-11-07 21:09:33 +0200

    update to 2.2.1

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -11,8 +11,10 @@ repositories {
 
 dependencies {
     implementation(project(":lib"))
-    implementation("com.pi4j:pi4j-core:2.2.0")
-    implementation("com.pi4j:pi4j-plugin-mock:2.2.0")
+    implementation("com.pi4j:pi4j-core:2.2.1")
+    implementation("com.pi4j:pi4j-plugin-raspberrypi:2.2.1")
+    implementation("com.pi4j:pi4j-plugin-pigpio:2.2.1")
+    implementation("com.pi4j:pi4j-plugin-mock:2.2.1")
     implementation("org.slf4j:slf4j-api:1.7.32")
     implementation("org.slf4j:slf4j-simple:1.7.32")
     testImplementation(kotlin("test"))
