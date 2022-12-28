7072cabab2b8eea15fccf1f8dd98b1771fc97a1f
Muhammad Hashim <msg@mhashim6.me>
2022-12-28 22:17:06 +0200

    2.3.0
    Signed-off-by: Muhammad Hashim <msg@mhashim6.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
-val libVersion by extra("2.3.0-SNAPSHOT")
+val libVersion by extra("2.3.0")
 
 group = "com.pi4j"
 version = libVersion
