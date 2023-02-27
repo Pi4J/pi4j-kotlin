976cf1f3be090a643a8bc74df1001866667c33dd
Muhammad Hashim <msg@mhashim6.me>
2023-02-27 11:30:22 +0200

    update I2C api and docs
    Signed-off-by: Muhammad Hashim <msg@mhashim6.me>

--- example/src/main/kotlin/I2CExample.kt
+++ example/src/main/kotlin/I2CExample.kt
@@ -20,20 +20,6 @@ import com.pi4j.ktx.pi4j
 import com.pi4j.ktx.utils.binStr
 import java.lang.Thread.sleep
 
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 
 private const val TCA9534_REG_ADDR_OUT_PORT: Int = 0x01
 private const val TCA9534_REG_ADDR_CFG: Int = 0x03
