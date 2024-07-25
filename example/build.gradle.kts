3958d0829b97f6897137ca46c7d0546700eb6332
Frank Delporte <frank@webtechie.be>
2024-07-25 13:37:02 +0200

    Added gpiod

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("com.pi4j:pi4j-core:2.6.0")
     implementation("com.pi4j:pi4j-plugin-raspberrypi:2.6.0")
     implementation("com.pi4j:pi4j-plugin-pigpio:2.6.0")
+    implementation("com.pi4j:pi4j-plugin-gpiod:2.6.0")
     implementation("com.pi4j:pi4j-plugin-linuxfs:2.6.0")
     implementation("com.pi4j:pi4j-plugin-mock:2.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
